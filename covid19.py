c81c37283b98db0485bfbe35df65a3958bde9263
Ahdian Rawuli <ahdian.rawuli@gmail.com>
2020-04-17 09:42:09 +0000

    add group checker

--- covid19.py
+++ covid19.py
@@ -5,12 +5,14 @@ list = response.json()
 final_d = []
 msg = ""
 for data in list:
-   kode_provi = data['attributes']['Kode_Provi']
+
    ## data corona ##
+   kode_provi = data['attributes']['Kode_Provi']
    provinsi = data['attributes']['Provinsi']
    kasus_posi = data['attributes']['Kasus_Posi']
    kasus_semb = data['attributes']['Kasus_Semb']
    kasus_meni = data['attributes']['Kasus_Meni']
+
    ## put togather as array##
    data_array = str(provinsi)+","+str(kasus_posi)+","+str(kasus_semb)+","+str(kasus_meni)
    try:
@@ -32,25 +34,21 @@ for data in list:
 
    ## check per array ##
    if f_posi != str(kasus_posi) or f_semb != str(kasus_semb) or f_meni != str(kasus_meni):
-
       val_posi = int(kasus_posi) - int(f_posi)
       if val_posi > 0:
          val_posi = " ( naik "+str(val_posi)+" angka )"
       else:
          val_posi = ""
-
       val_semb = int(kasus_semb) - int(f_semb)
       if val_semb > 0:
          val_semb = " ( naik "+str(val_semb)+" angka )"
       else:
          val_semb = ""
-
       val_meni = int(kasus_meni) - int(f_meni)
       if val_meni > 0:
          val_meni = " ( naik "+str(val_meni)+" angka )"
       else:
          val_meni = ""
-
       data_file = str(provinsi)+"|"+"<b>"+str(kasus_posi)+"</b>"+str(val_posi)+"|"+"<b>"+str(kasus_semb)+"</b>"+str(val_semb)+"|"+"<b>"+str(kasus_meni)+"</b>"+str(val_meni)
       final_d.append(data_file)
 
@@ -71,12 +69,10 @@ else:
       msg += "Meninggal "+split[3]+"%0A%0A"
 
    ## all indonesia statistic ##
-
    response = requests.get('https://api.kawalcorona.com/indonesia/')
    list = response.json()
 
    ## start messgae vars ##
-
    data_idn = "<b>[ Seluruh Indonesia ]</b>%0A"
    data_idn += "Positif <b>"+list[0]['positif']+"</b>%0A"
    data_idn += "Sembuh <b>"+list[0]['sembuh']+"</b>%0A"
@@ -84,32 +80,26 @@ else:
 
    link = "<a href='https://www.detik.com/tag/virus-corona'>Cek lebih lanjut..</a>"
    title = "<b>KawalCoronaIndonesia</b>%0A%0A"
-
    telegram_notif = title+msg+data_idn+link
 
    ## end message vars ##
-
    group_id = []
    remove_id = []
 
    ## get active group ID ##
    response = requests.get('https://api.telegram.org/bot1264034596:AAEzvf_ShnwYYkENitzGllz-ZzMmWfWRg80/getUpdates')
    list = response.json()
-
    for group in list['result']:
       id = group['message']['chat']['id']
       group_id.append(id)
-
    uniq = set(group_id)
    d_val = []
    for x in uniq:
       d_val.append(x)
-
    try:
       f = open("/project/corona_api/tmp/group","r")
       exists_array = json.loads(f.read())
       merge = exists_array+d_val
-
       uniq = set(merge)
       d_val = []
       for x in uniq:
@@ -121,7 +111,6 @@ else:
       f = open("/project/corona_api/tmp/group","w")
       f.write(str(d_val))
       f.close()
-
    f = open("/project/corona_api/tmp/group","r")
    json_group = json.loads(f.read())
    print json_group
@@ -134,10 +123,8 @@ else:
       elif response.status_code == 403 or response.status_code == 400:
          print "error "+str(g_id)
          remove_id.append(g_id)
-
    for trim in remove_id:
       json_group.remove(trim)
-
    f = open("/project/corona_api/tmp/group","w")
    f.write(str(json_group))
    f.close()
