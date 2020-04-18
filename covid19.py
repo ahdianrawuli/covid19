f667e7ad9d1085207927f2c25f80f743bd24e294
Ahdian Rawuli <ahdian.rawuli@gmail.com>
2020-04-18 16:43:59 +0000

    revisi call array

--- covid19.py
+++ covid19.py
@@ -1,131 +1,132 @@
+### COVID19 BOT ###
+### Sample Bot '1264034596:AAEzvf_ShnwYYkENitzGllz-ZzMmWfWRg80' ###
+
 import os, requests, json
 
 currentDirectory = os.path.dirname(os.path.realpath(__file__))
+list_result = str()
 response = requests.get('https://api.kawalcorona.com/indonesia/provinsi/')
 list = response.json()
-final_d = []
-msg = ""
-for data in list:
 
-   ## data corona ##
-   kode_provi = data['attributes']['Kode_Provi']
-   provinsi = data['attributes']['Provinsi']
-   kasus_posi = data['attributes']['Kasus_Posi']
-   kasus_semb = data['attributes']['Kasus_Semb']
-   kasus_meni = data['attributes']['Kasus_Meni']
+def open_file():
+   f = open(currentDirectory+"/data","r")
+   f_data = f.read()
+   f.close()
+   return f_data
 
-   ## put togather as array##
-   data_array = str(provinsi)+","+str(kasus_posi)+","+str(kasus_semb)+","+str(kasus_meni)
-   try:
-      f = open(currentDirectory+"/tmp/"+str(kode_provi),"r")
-      f.close()
-      print(str(kode_provi)+" exists")
-   except IOError:
-      f = open(currentDirectory+"/tmp/"+str(kode_provi),"w")
-      f.write(data_array)
-      f.close()
-      print(str(kode_provi)+" created")
+try:
+   open_file()
+except IOError:
+   with open(currentDirectory+'/data', 'w') as outfile:
+      json.dump(list, outfile)
+   open_file()
 
-   ## read from array file ##
-   f = open(currentDirectory+"/tmp/"+str(kode_provi),"r")
-   f_array = f.read().split(",")
-   f_posi = f_array[1]
-   f_semb = f_array[2]
-   f_meni = f_array[3]
+file_data = json.loads(open_file())
 
-   ## check per array ##
-   if f_posi != str(kasus_posi) or f_semb != str(kasus_semb) or f_meni != str(kasus_meni):
-      val_posi = int(kasus_posi) - int(f_posi)
-      if val_posi > 0:
-         val_posi = " ( naik "+str(val_posi)+" angka )"
-      else:
-         val_posi = ""
-      val_semb = int(kasus_semb) - int(f_semb)
-      if val_semb > 0:
-         val_semb = " ( naik "+str(val_semb)+" angka )"
-      else:
-         val_semb = ""
-      val_meni = int(kasus_meni) - int(f_meni)
-      if val_meni > 0:
-         val_meni = " ( naik "+str(val_meni)+" angka )"
-      else:
-         val_meni = ""
-      data_file = str(provinsi)+"|"+"<b>"+str(kasus_posi)+"</b>"+str(val_posi)+"|"+"<b>"+str(kasus_semb)+"</b>"+str(val_semb)+"|"+"<b>"+str(kasus_meni)+"</b>"+str(val_meni)
-      final_d.append(data_file)
+def returnNotMatches(a, b):
+    return [[x for x in a if x not in b], [x for x in b if x not in a]]
 
-      ## update data ##
-      f = open(currentDirectory+"/tmp/"+str(kode_provi),"w")
-      f.write(data_array)
-      f.close()
-      print(str(kode_provi)+" update")
+result = returnNotMatches(list,file_data)
 
-if not final_d:
-   print('no changes')
+## comapre array ##
+if result[0] == result[1]:
+   print "[ no changes ]"
 else:
-   for notif in final_d:
-      split = notif.split("|")
-      msg += "<b>[ "+split[0]+" ]</b>%0A"
-      msg += "Positif "+split[1]+"%0A"
-      msg += "Sembuh "+split[2]+"%0A"
-      msg += "Meninggal "+split[3]+"%0A%0A"
+   for x,y in zip(result[0],result[1]):
+      provinsi = "%0A<b>[ "+str(x['attributes']['Provinsi'])+" ]</b>%0A"
+      x_kasus_posi = x['attributes']['Kasus_Posi']
+      x_kasus_semb = x['attributes']['Kasus_Semb']
+      x_kasus_meni = x['attributes']['Kasus_Meni']
+
+      y_kasus_posi = y['attributes']['Kasus_Posi']
+      y_kasus_semb = y['attributes']['Kasus_Semb']
+      y_kasus_meni = y['attributes']['Kasus_Meni']
+
+      if x_kasus_posi != y_kasus_posi:
+         val = x_kasus_posi-y_kasus_posi
+         msg_kasus_posi = "Positif <b>"+str(x_kasus_posi)+"</b> ( naik "+str(val)+" angka )%0A"
+      else:
+         msg_kasus_posi = "Positif <b>"+str(x_kasus_posi)+"</b>%0A"
+      if x_kasus_semb != y_kasus_semb:
+         val = x_kasus_semb-y_kasus_semb
+         msg_kasus_semb = "Sembuh <b>"+str(x_kasus_semb)+"</b> ( naik "+str(val)+" angka )%0A"
+      else:
+         msg_kasus_semb = "Sembuh <b>"+str(x_kasus_semb)+"</b>%0A"
+      if x_kasus_meni != y_kasus_meni:
+         val = x_kasus_meni-y_kasus_meni
+         msg_kasus_meni = "Meninggal <b>"+str(x_kasus_meni)+"</b> ( naik "+str(val)+" angka ) %0A"
+      else:
+         msg_kasus_meni = "Meninggal <b>"+str(x_kasus_meni)+"</b>%0A"
+
+      list_result += provinsi+msg_kasus_posi+msg_kasus_semb+msg_kasus_meni
+
+   with open(currentDirectory+'/data', 'w') as outfile:
+      json.dump(list, outfile)
 
    ## all indonesia statistic ##
    response = requests.get('https://api.kawalcorona.com/indonesia/')
-   list = response.json()
+   list_idn = response.json()
 
-   ## start messgae vars ##
-   data_idn = "<b>[ Seluruh Indonesia ]</b>%0A"
-   data_idn += "Positif <b>"+list[0]['positif']+"</b>%0A"
-   data_idn += "Sembuh <b>"+list[0]['sembuh']+"</b>%0A"
-   data_idn += "Meninggal <b>"+list[0]['meninggal']+"</b>%0A%0A"
+   ## start message's vars ##
+   data_idn = "%0A<b>[ Seluruh Indonesia ]</b>%0A"
+   data_idn += "Positif <b>"+list_idn[0]['positif']+"</b>%0A"
+   data_idn += "Sembuh <b>"+list_idn[0]['sembuh']+"</b>%0A"
+   data_idn += "Meninggal <b>"+list_idn[0]['meninggal']+"</b>%0A%0A"
 
    link = "<a href='https://www.detik.com/tag/virus-corona'>Cek lebih lanjut..</a>"
-   title = "<b>KawalCoronaIndonesia</b>%0A%0A"
-   telegram_notif = title+msg+data_idn+link
+   title = "<b>KawalCoronaIndonesia</b>%0A"
 
-   ## end message vars ##
+   telegram_notif = title+list_result+data_idn+link
+
+   ## send to telegram in active ID ##
    group_id = []
    remove_id = []
-
-   ## get active group ID ##
    response = requests.get('https://api.telegram.org/bot1264034596:AAEzvf_ShnwYYkENitzGllz-ZzMmWfWRg80/getUpdates')
    list = response.json()
+
    for group in list['result']:
       id = group['message']['chat']['id']
       group_id.append(id)
+
    uniq = set(group_id)
    d_val = []
    for x in uniq:
       d_val.append(x)
+
    try:
-      f = open(currentDirectory+"/tmp/group","r")
+      f = open(currentDirectory+"/group","r")
       exists_array = json.loads(f.read())
       merge = exists_array+d_val
+
       uniq = set(merge)
       d_val = []
       for x in uniq:
          d_val.append(x)
-      f = open(currentDirectory+"/tmp/group","w")
+      ## store data to 'group' file ##
+      f = open(currentDirectory+"/group","w")
       f.write(str(d_val))
       f.close()
    except IOError:
-      f = open(currentDirectory+"/tmp/group","w")
+      f = open(currentDirectory+"/group","w")
       f.write(str(d_val))
       f.close()
-   f = open(currentDirectory+"/tmp/group","r")
-   json_group = json.loads(f.read())
-   print json_group
 
-   ## send to active group ##
+   f = open(currentDirectory+"/group","r")
+   json_group = json.loads(f.read())
+   print "Sending..\n"
    for g_id in json_group:
       response = requests.get('https://api.telegram.org/bot1264034596:AAEzvf_ShnwYYkENitzGllz-ZzMmWfWRg80/sendMessage?chat_id='+str(g_id)+'&parse_mode=HTML&disable_web_page_preview=false&text='+telegram_notif)
       if response.status_code == 200:
-         print "berhasil "+str(g_id)
+         print "[ "+str(g_id)+" ] Success.."
       elif response.status_code == 403 or response.status_code == 400:
-         print "error "+str(g_id)
+         print "[ "+str(g_id)+" ] Fail !"
+         ## remove failure group_id from array ##
          remove_id.append(g_id)
+
    for trim in remove_id:
       json_group.remove(trim)
-   f = open(currentDirectory+"/tmp/group","w")
+
+   ## store active group_id to group file ##
+   f = open(currentDirectory+"/group","w")
    f.write(str(json_group))
    f.close()
